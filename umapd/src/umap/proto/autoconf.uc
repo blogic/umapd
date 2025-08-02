blogic/umapd

commit f999f34cbacab93575b5d71e8ee9e1bf09c517b9

protocols: treat recognized but ignored CMDU as handled
In order to reduce the number of harmless unhandled CMDU warnings in the
logs, signal CMDUs as handled which we recognize but decide to ignore.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

umapd/src/umap/proto/autoconf.uc
@@ -47,18 +47,22 @@ const IAgentSession = {
 
 	debug: function (msg) {
 		log.debug(`autoconf: radio ${this.radio.address}: ${msg}`);
+		return true;
 	},
 
 	info: function (msg) {
 		log.info(`autoconf: radio ${this.radio.address}: ${msg}`);
+		return true;
 	},
 
 	warn: function (msg) {
 		log.warn(`autoconf: radio ${this.radio.address}: ${msg}`);
+		return true;
 	},
 
 	error: function (msg) {
 		log.error(`autoconf: radio ${this.radio.address}: ${msg}`);
+		return true;
 	},
 
 	transitionState: function (new_state) {
@@ -208,7 +212,7 @@ const IAgentSession = {
 		if (msg.type == defs.MSG_AP_AUTOCONFIGURATION_RESPONSE) {
 			// Ignore autoconf responses not belonging to our pending requests
 			if (!(msg.mid in this.midsInFlight))
-				return false;
+				return this.debug(`unexpected AP Auto-Configuration reponse`);
 
 			this.debug(`received AP Auto-Configuration response`);
 

umapd/src/umap/proto/capabilities.uc
@@ -123,7 +123,7 @@ const IProtoCapabilities = {
 	handle_cmdu: function (i1905lif, dstmac, srcmac, msg) {
 		// disregard CMDUs not directed to our AL
 		if (dstmac != model.address)
-			return false;
+			return true;
 
 		if (msg.type === defs.MSG_AP_CAPABILITY_QUERY) {
 			const reply = cmdu.create(defs.MSG_AP_CAPABILITY_REPORT, msg.mid);
@@ -170,7 +170,7 @@ const IProtoCapabilities = {
 		}
 		else if (msg.type === defs.MSG_AP_CAPABILITY_REPORT) {
 			if (!model.isController)
-				return false;
+				return true;
 
 			let i1905dev = model.lookupDevice(srcmac);
 
@@ -202,7 +202,7 @@ const IProtoCapabilities = {
 		}
 		else if (msg.type === defs.MSG_BACKHAUL_STA_CAPABILITY_REPORT) {
 			if (!model.isController)
-				return false;
+				return true;
 
 			let i1905dev = model.lookupDevice(srcmac);
 

umapd/src/umap/proto/scanning.uc
@@ -492,16 +492,20 @@ const IProtoScanning = {
 	handle_cmdu: function (i1905lif, dstmac, srcmac, msg) {
 		// disregard CMDUs not directed to our AL
 		if (dstmac != model.address)
-			return false;
+			return true;
 
 		if (msg.type === defs.MSG_CHANNEL_SCAN_REQUEST) {
 			const req = msg.get_tlv(defs.TLV_CHANNEL_SCAN_REQUEST);
 
-			if (!req)
-				return log.warn(`scanning: ignoring request message without scan request TLV`);
+			if (!req) {
+				log.warn(`scanning: ignoring request message without scan request TLV`);
+				return true;
+			}
 
-			if (!length(req.radios))
-				return log.warn(`scanning: ignoring request message without radios in request TLV`);
+			if (!length(req.radios)) {
+				log.warn(`scanning: ignoring request message without radios in request TLV`);
+				return true;
+			}
 
 			const ack = cmdu.create(defs.MSG_IEEE1905_ACK, msg.mid);
 			ack.send(i1905lif.i1905sock, model.address, srcmac);

umapd/src/umap/proto/topology.uc
@@ -170,7 +170,7 @@ const IProtoTopology = {
 
 			if (!al_mac || !if_mac) {
 				log.warn(`Ignoring incomplete topology discovery CMDU`);
-				return;
+				return true;
 			}
 
 			let dev = model.lookupDevice(al_mac);
@@ -203,10 +203,12 @@ const IProtoTopology = {
 		}
 		else if (msg.type == defs.MSG_TOPOLOGY_NOTIFICATION) {
 			if (!model.isController)
-				return false;
+				return true;
 
-			if (!al_mac)
-				return log.warn(`topology: ignoring notification without AL MAC`);
+			if (!al_mac) {
+				log.warn(`topology: ignoring notification without AL MAC`);
+				return true;
+			}
 
 			const query = cmdu.create(defs.MSG_TOPOLOGY_QUERY);
 
@@ -217,7 +219,7 @@ const IProtoTopology = {
 		else if (msg.type == defs.MSG_TOPOLOGY_QUERY) {
 			// Ignore queries destined to other nodes
 			if (dstmac != model.address)
-				return false;
+				return true;
 
 			let reply = cmdu.create(defs.MSG_TOPOLOGY_RESPONSE, msg.mid);
 
@@ -238,13 +240,13 @@ const IProtoTopology = {
 		else if (msg.type == defs.MSG_LINK_METRIC_QUERY) {
 			// Ignore queries destined to other nodes
 			if (dstmac != model.address)
-				return false;
+				return true;
 
 			let requested_metrics = msg.get_tlv(defs.TLV_LINK_METRIC_QUERY);
 
 			if (!requested_metrics) {
 				log.warn(`Ignoring incomplete link metric query CMDU`);
-				return false;
+				return true;
 			}
 
 			let reply = cmdu.create(defs.MSG_LINK_METRIC_RESPONSE, msg.mid);
@@ -263,13 +265,13 @@ const IProtoTopology = {
 		}
 		else if (msg.type == defs.MSG_TOPOLOGY_RESPONSE) {
 			if (!model.isController)
-				return false;
+				return true;
 
 			let devinfo = msg.get_tlv(defs.TLV_IEEE1905_DEVICE_INFORMATION);
 
 			if (!devinfo) {
 				log.warn(`Ignoring malformed topology response CMDU`);
-				return false;
+				return true;
 			}
 
 			let i1905dev = model.addDevice(devinfo.al_mac_address);
@@ -298,7 +300,7 @@ const IProtoTopology = {
 
 				if (!transmitter_al_mac_address) {
 					log.warn(`Ignoring malformed metrics reply CMDU`);
-					return false;
+					return true;
 				}
 
 				push((tlvs_by_al_address ??= {})[transmitter_al_mac_address] ??= [], tlv);
@@ -316,7 +318,7 @@ const IProtoTopology = {
 		else if (msg.type == defs.MSG_HIGHER_LAYER_QUERY) {
 			// Ignore queries destined to other nodes
 			if (dstmac != model.address)
-				return false;
+				return true;
 
 			let reply = cmdu.create(defs.MSG_HIGHER_LAYER_RESPONSE, msg.mid);
 
@@ -341,8 +343,8 @@ const IProtoTopology = {
 			let i1905dev = model.lookupDevice(srcmac);
 
 			if (!i1905dev) {
-				log.warn('Ignoring infrastructure metrics from unknown device %s', srcmac);
-				return false;
+				log.debug('Ignoring infrastructure metrics from unknown device %s', srcmac);
+				return true;
 			}
 
 			i1905dev.updateTLVs(msg.get_tlvs_raw());

commit 7250e3a41d03bbcc4d3e20144387061f1b6d3846

autoconf: fix handling multiple sessions
Do not handle messages potentially meant for another session

Fixes: f999f34cbaca ("protocols: treat recognized but ignored CMDU as handled")
Signed-off-by: Felix Fietkau <nbd@nbd.name>

umapd/src/umap/proto/autoconf.uc
@@ -211,8 +211,10 @@ const IAgentSession = {
 	handle_cmdu: function (i1905lif, dstmac, srcmac, msg) {
 		if (msg.type == defs.MSG_AP_AUTOCONFIGURATION_RESPONSE) {
 			// Ignore autoconf responses not belonging to our pending requests
-			if (!(msg.mid in this.midsInFlight))
-				return this.debug(`unexpected AP Auto-Configuration reponse`);
+			if (!(msg.mid in this.midsInFlight)) {
+				this.debug(`unexpected AP Auto-Configuration reponse`);
+				return false;
+			}
 
 			this.debug(`received AP Auto-Configuration response`);
 
@@ -242,16 +244,18 @@ const IAgentSession = {
 			return true;
 		}
 		else if (msg.type == defs.MSG_AP_AUTOCONFIGURATION_WSC) {
-			if (this.state != 'config_request')
-				return this.warn(`received WSC message while not in config request state`);
-
 			if (srcmac != this.controller.address)
 				return this.warn(`received WSC message from unexpected address ${srcmac}, expected ${this.controller.address}`);
 
 			const radio_id = msg.get_tlv(defs.TLV_AP_RADIO_IDENTIFIER);
 
-			if (radio_id != this.radio.address)
-				return this.debug(`ignoring WSC message for different radio ${radio_id}`);
+			if (radio_id != this.radio.address) {
+				this.debug(`ignoring WSC message for different radio ${radio_id}`);
+				return false;
+			}
+
+			if (this.state != 'config_request')
+				return this.warn(`received WSC message while not in config request state`);
 
 			const wscFrames = msg.get_tlvs(defs.TLV_WSC);
 
@@ -277,8 +281,10 @@ const IAgentSession = {
 			return true;
 		}
 		else if (msg.type == defs.MSG_AP_AUTOCONFIGURATION_RENEW) {
-			if (this.state != 'idle')
-				return this.warn(`ignoring renew request while not idle`);
+			if (this.state != 'idle') {
+				this.debug(`ignoring renew request while not idle`);
+				return false;
+			}
 
 			const al_mac = msg.get_tlv(defs.TLV_IEEE1905_AL_MAC_ADDRESS);
 